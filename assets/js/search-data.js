a90c17464c9ae09c7a8556bdf2acfba19a9f970d
samuelkoovely <samuelkoovely@gmail.com>
2025-05-31 11:22:55 +0000

    Deploying to gh-pages from @ samuelkoovely/samuelkoovely.github.io@ad28136fd1d9514623f41997fbba712b0acd3306 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -24,8 +24,8 @@ ninja.data = [{
             window.location.href = "/publications/";
           },
         },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
+          title: "Projects",
+          description: "A non-comprehensive list of things that I have been working on.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
