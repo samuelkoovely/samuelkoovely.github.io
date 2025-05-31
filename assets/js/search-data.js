8b3c73d71833d4bd4d12d1b602e64dfd15dfd1e1
samuelkoovely <samuelkoovely@gmail.com>
2025-05-31 09:32:15 +0000

    Deploying to gh-pages from @ samuelkoovely/samuelkoovely.github.io@a4f4291c4209311061b073171f819731ebb3167c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -437,51 +437,31 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-community-detection-in-temporal-networks",
+          title: 'Community Detection in Temporal Networks',
+          description: "Ongoing",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+              window.location.href = "/projects/CD_in_TN/";
+            },},{id: "projects-comic-tree-model",
+          title: 'COMIC-Tree Model',
+          description: "Ongoing",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+              window.location.href = "/projects/COMIC_Tree/";
+            },},{id: "projects-msc-thesis",
+          title: 'MSc Thesis',
+          description: "Completed",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+              window.location.href = "/projects/MSc_Thesis/";
+            },},{id: "projects-seminar-on-machine-learning",
+          title: 'Seminar on Machine Learning',
+          description: "Completed",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
+              window.location.href = "/projects/Seminar_ML/";
+            },},{id: "projects-seminar-on-markov-models",
+          title: 'Seminar on Markov Models',
+          description: "Completed",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
+              window.location.href = "/projects/Seminar_Markov/";
             },},{
         id: 'social-email',
         title: 'email',
---
+++ projects/Centrality_CitationNet.md
@@ -0,0 +1,12 @@
+<!--
+---
+layout: page
+title: Centrality Citation Network
+description: Ongoing
+img: assets/img/projects_sketches/Centrality_CitationNet.jpeg
+importance: 4
+category: Others
+---
+
+I am learning about previously developed metrics of impact of scientific articles and researchers. I am thinking about new possible methods that would better highlight the temporal evolution of scientific relevance.
+-->
